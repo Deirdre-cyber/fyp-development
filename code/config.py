2fca8d0b722e5f53b23208668ed5998bb9c0f692
Deirdre Laptop <dleeee333@gmail.com>
2024-01-31 14:20:16 +0000

    Added filepaths to config file and updated variable names in ntebook for clarity

--- code/config.py
+++ code/config.py
@@ -1,1 +1,3 @@
-# config files (costants, parameters, paths, etc.)
+""" Configuration file for the project."""
+DATA_PATH = '../data/raw/donateacry_corpus_cleaned_and_updated_data/'
+HUNGRY_TEST_SAMPLE = '/0a983cd2-0078-4698-a048-99ac01eb167a-1433917038889-1.7-f-04-hu.wav'
