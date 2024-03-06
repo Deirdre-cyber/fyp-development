a2b7b5d23865c0628441c2a7ee65f809d692b74c
Deirdre Laptop <dleeee333@gmail.com>
2024-03-06 16:02:55 +0000

    FIN-55 : Resolved issue

--- code/config.py
+++ code/config.py
@@ -1,6 +1,6 @@
 """ Configuration file for the project."""
 ROOT_DIR_PATH = '../data/raw/'
-PROCESSED_DIR = '/data/processed/'
+PROCESSED_DIR = '../data/processed/'
 TRAIN_DIR = '../data/processed/train'
 VAL_DIR = '../data/processed/val'
 TEST_DIR = '../data/processed/test'
